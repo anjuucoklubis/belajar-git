d4c3d9af80880a364f62a6cc7bd758fbef5ddc55
anjuucoklubis <anjuucoklubis@gmail.com>
2023-10-04 17:31:30 +0700

    fix: bug login 5

--- login.js
+++ login.js
@@ -7,6 +7,7 @@ export default function login() {
       <text>commit login 2</text>
       <text>commit login 3</text>
       <text>commit login 4</text>
+      <text>commit login 5</text>
     </div>
   );
 }
